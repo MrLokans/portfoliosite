d0cc7399d02fd7669d0cc2fc1d644e41583fe366
MrLokans <trikster1911@gmail.com>
2016-01-11 08:54:18 +0300

    Removed old static html file. Models extanded,

--- personal_site/blog/models.py
+++ personal_site/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+
+    value = models.CharField(max_length=60)
+
+
+class Book(models.Model):
+
+    author = models.CharField(max_length=300)
+    title = models.CharField(max_length=200)
+    tag = models.ManyToManyField(Tag)
+
+
+class BookNote(models.Model):
+
+    book = models.ForeignKey('Book')
+    text = models.TextField()
+    type = models.IntegerField()
