46f9e530ce0572d551c9ac38d8742f1dd7dbd3fa
MrLokans <trikster1911@gmail.com>
2016-05-22 08:51:08 +0300

    Book notes also displayed inbook list API view.

--- personal_site/books/api/serializers.py
+++ personal_site/books/api/serializers.py
@@ -3,7 +3,30 @@ from rest_framework import serializers
 from books.models import BookNote, Book
 
 
+class BookNoteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = BookNote
+        fields = [
+            'id',
+            'text',
+        ]
+
+
 class BookListSerializer(serializers.ModelSerializer):
+    notes = serializers.SerializerMethodField()
 
     class Meta:
         model = Book
+        fields = [
+            'id',
+            'title',
+            'percentage',
+            'rating',
+            'notes'
+        ]
+
+    def get_notes(self, obj):
+        notes = BookNote.objects.filter(book=obj)
+        notes = BookNoteSerializer(notes, many=True).data
+        return notes
