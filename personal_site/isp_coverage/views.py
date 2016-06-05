a531fb7aa134bc721f4e9e08b65e59ff599caaa6
MrLokans <trikster1911@gmail.com>
2016-06-05 20:19:07 +0300

    Data from unet.by is now rendered.

--- personal_site/isp_coverage/views.py
+++ personal_site/isp_coverage/views.py
@@ -7,6 +7,7 @@ from django.views.generic import TemplateView
 
 BYFLY_CSV_FILE = os.path.abspath(os.path.join('isp_coverage', "byfly.csv"))
 MTS_CSV_FILE = os.path.abspath(os.path.join('isp_coverage', "mts.csv"))
+UNET_CSV_FILE = os.path.abspath(os.path.join('isp_coverage', "unet.csv"))
 
 
 class MapView(TemplateView):
@@ -20,5 +21,10 @@ class MapView(TemplateView):
         with open(MTS_CSV_FILE, 'r') as csv_file:
             reader = csv.reader(csv_file)
             mts_dots = [row for row in reader if row[0] != "longitude"]
+        with open(UNET_CSV_FILE, 'r') as csv_file:
+            reader = csv.reader(csv_file)
+            unet_dots = [row for row in reader if row[0] != "longitude"]
         # coords = [()]
-        return render(request, "isp_map.html", {"byfly_coords": byfly_dots, "mts_coords": mts_dots})
+        return render(request, "isp_map.html", {"byfly_coords": byfly_dots,
+                                                "mts_coords": mts_dots,
+                                                "unet_coords": unet_dots})
