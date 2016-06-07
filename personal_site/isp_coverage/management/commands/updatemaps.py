31e190210af3c35f679943e0be33cc69e11c548a
MrLokans <trikster1911@gmail.com>
2016-06-07 21:50:42 +0300

    Management command updated to fill database with provider coordinates data.

--- personal_site/isp_coverage/management/commands/updatemaps.py
+++ personal_site/isp_coverage/management/commands/updatemaps.py
@@ -1,8 +1,14 @@
+import logging
+
 from django.core.management.base import BaseCommand
 
 from by_isp_coverage import ByflyParser, MTS_Parser, CSV_Exporter
 
+from isp_coverage.models import Provider, ProviderCoordinate
 from isp_coverage.views import MTS_CSV_FILE, BYFLY_CSV_FILE
+
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -10,12 +16,28 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
 
+        ProviderCoordinate.objects.all().delete()
+        byfly_provider, _ = Provider.objects.get_or_create(name="byfly", url="http://byfly.by")
+        mts_provider, _ = Provider.objects.get_or_create(name="mts", url="http://mts.by")
+        unet_provider, _ = Provider.objects.get_or_create(name="unet", url="http://unet.by")
+
         byfly_parser = ByflyParser()
         mts_parser = MTS_Parser()
-        exporter = CSV_Exporter()
+        # exporter = CSV_Exporter()
 
+        logger.info("Obtaining data from byfly.")
         byfly_data = byfly_parser.get_points()
-        mts_data = mts_parser.get_points()
+        logger.info("Saving data from byfly to database.")
+        for point in byfly_data:
+            coord = ProviderCoordinate.fromnamedtuple(point, byfly_provider)
+            coord.save()
 
-        exporter.export_points(MTS_CSV_FILE, mts_data)
-        exporter.export_points(BYFLY_CSV_FILE, byfly_data)
+        logger.info("Obtaining data from mts.")
+        mts_data = mts_parser.get_points()
+        logger.info("Saving data from mts to database.")
+        for point in mts_data:
+            coord = ProviderCoordinate.fromnamedtuple(point, mts_provider)
+            coord.save()
+
+        # exporter.export_points(MTS_CSV_FILE, mts_data)
+        # exporter.export_points(BYFLY_CSV_FILE, byfly_data)
