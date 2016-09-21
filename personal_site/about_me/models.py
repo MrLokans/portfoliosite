d2f8d68b4a7800c20d462f7247af52e8416ced4d
MrLokans <trikster1911@gmail.com>
2016-09-21 20:39:03 +0300

    Basic models concerning about_me page added.

---
+++ personal_site/about_me/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+
+class Technology(models.Model):
+    """Model, representing single technology or skill"""
+
+    NOVICE = 1
+    INTERMEDIATE = 2
+    ADVANCED = 3
+
+    MASTERY_CHOICES = (
+        (NOVICE, 'Novice'),
+        (INTERMEDIATE, 'Intermediate'),
+        (ADVANCED, 'Advanced')
+    )
+
+    name = models.CharField(max_length=120)
+    general_description = models.TextField()
+    mastery_level = models.IntegerField(choices=MASTERY_CHOICES,
+                                        default=NOVICE)
+
+    def __str__(self):
+        return '<Skill: "{}">'.format(self.name)
+
+
+class Project(models.Model):
+    """Model, representing single project,
+    with a list of technologies used and appropriate
+    links
+    """
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    technologies = models.ManyToManyField(Technology)
+
+    def __str__(self):
+        return '<Project "{}"'.format(self.title)
+
+
+class ProjectLink(models.Model):
+    """Model, representing single link of the project"""
+    link = models.URLField()
+    name = models.CharField(max_length=120)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '<Project: {} - {}>'.format(self.project, self.link)
