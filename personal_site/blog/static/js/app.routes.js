7927ccd8e7ebdedfd3f1270a752c41e71b865680
MrLokans <trikster1911@gmail.com>
2016-09-21 18:56:43 +0300

    Now / response is redirected to /about_me page.

--- personal_site/blog/static/js/app.routes.js
+++ personal_site/blog/static/js/app.routes.js
@@ -10,26 +10,24 @@
     function config($routeProvider, $locationProvider){
         // $locationProvider.html5Mode(true);
         $routeProvider
-            // .when('/', {
-            //     template: 'test'
-            // })
             .when('/projects', {
                 templateUrl: 'projects.html',
                 controller: "ProjectController"
             })
-
             .when('/blog', {
                 templateUrl: 'blog.html',
             })
-
             .when('/books', {
                 templateUrl: 'books.html'
             })
-
             .when('/about_me', {
                 templateUrl: 'about_me.html',
                 controller: "AboutController",
                 controllerAs: "vm"
-        }).otherwise('/');
+            })
+            .when('/', {
+                redirectTo: '/about_me'
+            }).
+            otherwise('/');
     }
 })();
