4a13cc8d14579ce44e5beafc7c0ce353eb17904e
MrLokans <trikster1911@gmail.com>
2016-10-01 12:36:32 +0300

    API permissions updated for unauthenticated users. Closes #21.

--- personal_site/about_me/api/views.py
+++ personal_site/about_me/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.permissions import AllowAny
 
 from about_me.api.serializers import TechnologySerializer, ProjectSerializer
 from about_me.models import Project, Technology
@@ -8,6 +9,7 @@ from about_me.models import Project, Technology
 class TechnologyListAPIView(ListAPIView):
     serializer_class = TechnologySerializer
     filter_backends = [SearchFilter, OrderingFilter]
+    permission_classes = (AllowAny, )
     ordering = '-mastery_level'
     search_fields = ['name', ]
 
@@ -19,6 +21,7 @@ class TechnologyListAPIView(ListAPIView):
 class ProjectListAPIView(ListAPIView):
     serializer_class = ProjectSerializer
     filter_backends = [SearchFilter, OrderingFilter]
+    permission_classes = (AllowAny, )
     ordering = 'title'
     search_fields = ['title', ]
 
--- personal_site/blog/api/views.py
+++ personal_site/blog/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.permissions import AllowAny
 
 from blog.models import Post
 from blog.api.serializers import PostListSerializer
@@ -8,6 +9,7 @@ from blog.api.serializers import PostListSerializer
 class PostListAPIView(generics.ListAPIView):
     serializer_class = PostListSerializer
     filter_backends = [SearchFilter, OrderingFilter]
+    permission_classes = (AllowAny, )
     ordering = 'title'
     # TODO: search by book's notes content
     search_fields = ['title', ]
@@ -20,6 +22,7 @@ class PostListAPIView(generics.ListAPIView):
 
 class PostDetailsAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
+    permission_classes = (AllowAny, )
     serializer_class = PostListSerializer
 
 
