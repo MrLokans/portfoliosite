d6fce2e920adf7e9b39d9fb4854c56c7851a5d70
MrLokans <trikster1911@gmail.com>
2016-10-15 20:58:51 +0300

    Extra settings added. Fixtures are now loaded during container startup.

--- personal_site/personal_site/settings/jenkins.py
+++ personal_site/personal_site/settings/jenkins.py
@@ -1,7 +1,7 @@
 from .base import *
 
 
-SECRET_KEY = os.environ.get('PERSONAL_SITE_SECRET_KEY')
+SECRET_KEY = os.environ.get('PERSONAL_SITE_SECRET_KEY', 'jenkins')
 DEBUG = True
 
 ALLOWED_HOSTS = ("*",)
