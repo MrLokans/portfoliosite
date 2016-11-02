1ba2bc5614c88a2cb0062da8f9174d805d2984be
MrLokans <trikster1911@gmail.com>
2016-11-02 20:06:24 +0300

    Now production server uses sqlite database (currently there is no neccessity to use humongous postgresql).

--- personal_site/personal_site/settings/base.py
+++ personal_site/personal_site/settings/base.py
@@ -95,7 +95,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.environ.get('STATIC_DIR',
+                             os.path.join(BASE_DIR,
+                                          'staticfiles'))
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
--- personal_site/personal_site/settings/prod.py
+++ personal_site/personal_site/settings/prod.py
@@ -9,11 +9,7 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('PERSONAL_SITE_DB_NAME'),
-        'USER': os.environ.get('PERSONAL_SITE_DB_USER'),
-        'PASSWORD': os.environ.get('PERSONAL_SITE_DB_PASSWORD'),
-        'HOST': os.environ.get('PERSONAL_SITE_DB_HOST', 'localhost'),
-        'PORT': os.environ.get('PERSONAL_SITE_DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
