1263f4230d1ed4eced88d88c5182522b6bc560c8
MrLokans <trikster1911@gmail.com>
2016-12-01 08:17:01 +0300

    All static files are now removed from the back-end.

--- backend/blog/static/js/app.config.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    'use strict';
-    angular.module('andersblog.config')
-        .config(function($httpProvider, $locationProvider){
-            $httpProvider.interceptors.push('AuthInterceptor');
-        });
-    
-})();
