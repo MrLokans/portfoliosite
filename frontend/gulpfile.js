e0c6e19dd7b6cc46d8536dc65bfb92d3503b1fde
MrLokans <trikster1911@gmail.com>
2016-12-03 19:18:48 +0300

    Angular JS scripts bundling added.

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,28 +1,38 @@
 var gulp = require('gulp');
 var minify = require('gulp-minify');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var watch = require('gulp-watch');
 var cleanCSS = require('gulp-clean-css');
+var ngConstant = require('gulp-ng-constant');
  
-gulp.task('minify-css', function() {
-  return gulp.src(['./personal_site/blog/static/css/*.css',
-                   '!./personal_site/blog/static/css/*.min.css'])
-    .pipe(cleanCSS())
-    .pipe(gulp.dest('./outcss'));
+
+gulp.task('concat-angular', function(){
+    gulp.src(['./js/app/app.js', './js/app/*.js', './js/app/**/*.js'])
+        .pipe(concat('angular.app.js'))
+        .pipe(gulp.dest('./js/dist/'));
 });
 
-gulp.task('compress', function() {
-  gulp.src('lib/*.js')
-    .pipe(minify({
-        ext:{
-            src:'-debug.js',
-            min:'.js'
-        },
-        exclude: ['tasks'],
-        ignoreFiles: ['.combo.js', '-min.js']
-    }))
-    .pipe(gulp.dest('dist'))
-});
 
+gulp.task('config:dev', function(){
+    gulp.src('js/configs/dev.json')
+        .pipe(ngConstant({
+              "name": "andersblog.constants",
+              "deps": [],
+              "wrap": false,
+              "wrapHeader": "(function(){",
+              "wrapFooter": "});"
+            }))
+        .pipe(rename('app.constants.js'))
+        .pipe(gulp.dest('./js/app/'));
+});
 
 gulp.task('default', function(){
-    console.log('Hello!');
+    gulp.run('config:dev');
+    gulp.run('concat-angular');
+
+    gulp.watch('js/**', function(event){
+        gulp.run('config:dev');
+        gulp.run('concat-angular');
+    });
 });
