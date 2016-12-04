6683136691c5a4410b802703abad88d1fce7ec3b
MrLokans <trikster1911@gmail.com>
2016-12-04 20:21:14 +0300

    gulp pipeline updated.

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,18 +1,38 @@
 var gulp = require('gulp');
-var minify = require('gulp-minify');
+var clean = require('gulp-clean');
+var cleanCSS = require('gulp-clean-css');
 var concat = require('gulp-concat');
+var inject = require('gulp-inject');
+var minify = require('gulp-minify');
+var ngConstant = require('gulp-ng-constant');
 var rename = require('gulp-rename');
 var watch = require('gulp-watch');
-var cleanCSS = require('gulp-clean-css');
-var inject = require('gulp-inject');
-var ngConstant = require('gulp-ng-constant');
+var webserver = require('gulp-webserver');
+
 var bowerFiles = require('main-bower-files');
+
+
+gulp.task('clean:build', function(){
+    // TODO: this causes errors due to async removal
+    return gulp.src('./build/', {read: false}).pipe(clean());
+});
  
 
-gulp.task('concat-angular', function(){
-    gulp.src(['./js/app/app.js', './js/app/*.js', './js/app/**/*.js'])
+gulp.task('webserver', function() {
+  gulp.src('./build/')
+    .pipe(webserver({
+      port: 8090,
+      livereload: true,
+      directoryListing: false,
+      open: true
+    }));
+});
+
+
+gulp.task('concat:angular', function(){
+    gulp.src(['./js/app/app.js', './build/app.constants.js', './js/app/*.js', './js/app/**/*.js'])
         .pipe(concat('angular.app.js'))
-        .pipe(gulp.dest('./js/dist/'));
+        .pipe(gulp.dest('./build/'));
 });
 
 
@@ -26,21 +46,39 @@ gulp.task('config:dev', function(){
               "wrapFooter": "});"
             }))
         .pipe(rename('app.constants.js'))
-        .pipe(gulp.dest('./js/app/'));
+        .pipe(gulp.dest('./build/'));
 });
 
-gulp.task('inject-bower', function(){
+gulp.task('copy:bower-components', function(){
+    gulp.src(['./bower_components/**/*'])
+        .pipe(gulp.dest('./build/bower_components/'));
+});
+
+gulp.task('copy:angular', function(){
+    // Copies angular js scripts
+    gulp.src(['./js/app/**/*'])
+        .pipe(gulp.dest('./build/js/app/'));
+})
+
+gulp.task('inject:bower', function(){
+    // Inject bower dependencies
     gulp.src('./index.html')
         .pipe(inject(gulp.src(bowerFiles(), {read: false}), {name: 'bower'}))
         .pipe(gulp.dest('./build'));
 });
 
-gulp.task('default', function(){
+gulp.task('build-dev', function(){
+    gulp.run('copy:bower-components');
     gulp.run('config:dev');
-    gulp.run('concat-angular');
+    gulp.run('concat:angular');
+    gulp.run('inject:bower');
+});
 
+gulp.task('default', function(){
+    // gulp.run('clean:build');
+    gulp.run('build-dev')
+    gulp.run('webserver');
     gulp.watch('js/**', function(event){
-        gulp.run('config:dev');
-        gulp.run('concat-angular');
+        gulp.run('build-dev')
     });
 });
