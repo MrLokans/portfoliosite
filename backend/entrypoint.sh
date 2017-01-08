efb01b9195d629c3c0e24a399a7b05ac4e74645d
MrLokans <trikster1911@gmail.com>
2017-01-08 15:18:17 +0300

    Backend entrypoint steps updated.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
+python manage.py makemigrations
+python manage.py migrate --run-syncdb
 python manage.py loaddata about_me/fixtures/fixtures.yaml
 python manage.py collectstatic --no-input
-python manage.py migrate
 gunicorn -w 4 -b :8000 personal_site.wsgi:application --reload
