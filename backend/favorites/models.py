840447353d19b61dbc462348abd273c8801a41fe
MrLokans <trikster1911@gmail.com>
2017-01-08 12:26:58 +0300

    Name field added to the favorites link.

--- backend/favorites/models.py
+++ backend/favorites/models.py
@@ -8,6 +8,9 @@ class FavoriteLink(TimeTrackable):
     This model is used to store link to any
     interesting resource on the internet
     """
-
+    name = models.CharField(max_length=120, blank=True)
     url = models.URLField(max_length=320, unique=True)
     comment = models.TextField(blank=True)
+
+    def __repr__(self):
+        return 'FavoriteLink(url={})'.format(self.url)
