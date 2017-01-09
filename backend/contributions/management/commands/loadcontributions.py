00954504d519f28337e4e2ee49139d6bc47ec1e6
MrLokans <trikster1911@gmail.com>
2017-01-09 19:45:26 +0300

    Database IntegrityError handling added.

--- backend/contributions/management/commands/loadcontributions.py
+++ backend/contributions/management/commands/loadcontributions.py
@@ -3,7 +3,7 @@ import logging
 from django.core.management.base import (
     BaseCommand,
 )
-from django.db import transaction
+from django.db import transaction, IntegrityError
 from contributions.models import Contribution
 from contributions.services import ContributionObtainer
 
@@ -20,10 +20,13 @@ class Command(BaseCommand):
                             help='Github username')
 
     def handle(self, *args, **kwargs):
-        with transaction.atomic():
-            obtainer = ContributionObtainer()
-            Contribution.objects.all().delete()
-            conts = obtainer.get_all_contributions(kwargs['username'])
-            conts = [Contribution.from_contribution_object(c)
-                     for c in set(conts)]
-            Contribution.objects.bulk_create(conts)
+        try:
+            with transaction.atomic():
+                obtainer = ContributionObtainer()
+                Contribution.objects.all().delete()
+                conts = obtainer.get_all_contributions(kwargs['username'])
+                conts = [Contribution.from_contribution_object(c)
+                         for c in set(conts)]
+                Contribution.objects.bulk_create(conts)
+        except IntegrityError as e:
+            logger.exception("Database error.")
