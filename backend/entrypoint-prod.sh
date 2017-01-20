12aa56780dc0a436d71b68e14b0430ec41926df0
MrLokans <trikster1911@gmail.com>
2017-01-20 20:49:25 +0300

    production entrypoint for the backend service added.

---
+++ backend/entrypoint-prod.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+python manage.py makemigrations
+python manage.py migrate --run-syncdb
+python manage.py loaddata about_me/fixtures/fixtures.yaml
+python manage.py collectstatic --no-input
+gunicorn -w 4 -b :8000 personal_site.wsgi:application --access-logfile - --error-logfile -
