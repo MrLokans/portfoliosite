d80ae69ec1e7f67bdc834024ff2d4c5e3ac93e19
MrLokans <trikster1911@gmail.com>
2017-02-01 20:53:20 +0300

    Redis container added as a dependency. Missing links added.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 set -e
 
-python manage.py makemigrations
-python manage.py migrate --run-syncdb
-python manage.py loaddata about_me/fixtures/fixtures.yaml
+BACKEND_USER=bloguser
+
+echo "Applying migrations"
+su "$BACKEND_USER" -c "python manage.py makemigrations"
+su "$BACKEND_USER" -c "python manage.py migrate --run-syncdb"
+echo "Loading initial fixtures"
+su "$BACKEND_USER" -c "python manage.py loaddata about_me/fixtures/fixtures.yaml"
+echo "Collecting static"
+# chown -R $BACKEND_USER:$BACKEND_USER static
 python manage.py collectstatic --no-input
-gunicorn -w 4 -b :8000 personal_site.wsgi:application --reload
+
+if [[ $@ == **prod** ]]
+then
+    echo "Using prod configuration."
+    su "$BACKEND_USER" -c "gunicorn -w 4 -b :8000 personal_site.wsgi:application --access-logfile - --error-logfile -"
+else
+    echo "Using dev configuration"
+    su "$BACKEND_USER" -c "gunicorn -w 4 -b :8000 personal_site.wsgi:application --reload"
+fi
