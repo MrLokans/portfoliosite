04cc39c6bef80aba2e3d7407beecae1120ea987e
MrLokans <MrLokans@gmail.com>
2017-06-16 21:46:01 +0300

    Production django config uses environemnt variables to get database connection data.

--- backend/personal_site/settings/prod.py
+++ backend/personal_site/settings/prod.py
@@ -29,7 +29,11 @@ RAVEN_CONFIG = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['DATABASE_HOST'],
+        'PORT': os.environ['POSTGRES_PORT'],
     }
 }
