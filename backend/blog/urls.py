e18401279771913cf1ff39312c036dd3ca24231c
MrLokans <MrLokans@gmail.com>
2018-02-25 15:27:14 +0300

    #68: Blog posts API removed as it will be rendered server-side.

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from .api.views import PostListAPIView, PostDetailsAPIView
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('<int:pk>/', PostDetailsAPIView.as_view(), name='detail'),
-    path('/', PostListAPIView.as_view(), name="list"),
+
 ]
