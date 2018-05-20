692a776540f4df431714b02c8649c46e86a1dc58
Cory_Flores <trikster1911@gmail.com>
2018-05-20 17:00:45 +0300

    Added basic continuous delivery set-up.
    - docker_push.sh script added, providing a way to build and push the
    backend docker image
    - initial travis config added, tracking the develop branch and launching
    the beforementioned script.

---
+++ docker_push.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# This script is used to build and push the back-end docker image
+# to the private registry, where it may be picked up afterwards
+
+set -e
+
+DOCKERFILE_DIR=backend
+DOCKER_REGISTRY_URL=registry.mrlokans.com:5000
+
+# Union of version, current date and git revision
+IMAGE_VERSION="$(cat VERSION)-$(date +'%Y-%m-%d_%H-%M-%S')-$(git rev-parse --short --verify HEAD)"
+IMAGE_NAME=personal_site
+FULL_IMAGE_NAME=$DOCKER_REGISTRY_URL/$IMAGE_NAME:$IMAGE_VERSION
+LATEST_IMAGE_NAME=$DOCKER_REGISTRY_URL/$IMAGE_NAME:latest
+
+if [ -z "$DOCKER_USERNAME" ]; then
+    echo "DOCKER_USERNAME environment variable is not set, failing."
+    exit 1
+fi
+
+if [ -z "$DOCKER_PASSWORD" ]; then
+    echo "DOCKER_PASSWORD environment variable is not set, failing."
+    exit 1
+fi
+
+echo "Image version: $IMAGE_VERSION"
+
+echo "Logging in into the docker registry."
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin $DOCKER_REGISTRY_URL
+
+echo "Build docker image."
+docker build $DOCKERFILE_DIR -t "$FULL_IMAGE_NAME"
+echo "Tagging the latest version."
+docker tag $FULL_IMAGE_NAME $LATEST_IMAGE_NAME
+echo "Pushing images to the registry."
+docker push $FULL_IMAGE_NAME
+docker push $LATEST_IMAGE_NAME
