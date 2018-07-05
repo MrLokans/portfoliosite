dc2706610b442cee02f1fbe4e9c4f132c357b905
MrLokans <trikster1911@gmail.com>
2018-07-05 17:25:59 +0300

    Ignored files list updated. Fixed missing dev settings.

---
+++ personal_site/settings/dev.py
@@ -0,0 +1,22 @@
+from .base import *  # noqa
+
+DEBUG = True
+
+ALLOWED_HOSTS = ("*",)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        }
+    }
+}
